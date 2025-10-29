bdf612b75ad38c80ff772ebfb9690d15134fee67
Juan Eslava Herraiz <juaneslavahe@gmail.com>
2025-10-29 19:19:28 +0100

    solo vpc fincional

--- script1.sh
+++ script1.sh
@@ -1,8 +1,14 @@
 #Creo y devuelvo su id
-VPC_ID = $(aws ec2 create --vpc --cdir-block 192.168.0.0/24 \
-    --tag-specifications 'ResourceType=vpc, Tags=ResourceType=vpc, Tags=[Key=Name,Value=MyVpc]' \
-    --query Vpc.VpcId --output text)
+VPC_ID=$(aws ec2 create-vpc \
+    --cidr-block 192.168.0.0/24 \
+    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
+    --query 'Vpc.VpcId' \
+    --output text)
 
+#MUESTRO EL ID DE LA VPC
+echo $VPC_ID
+
+#
 aws ec2 modify-vpc-attribute \
     --vpc-id $VPC_ID \
-    --enable-dns-hostnames "(\"Value\":true)"
+    --enable-dns-hostnames "{\"Value\":true}"
